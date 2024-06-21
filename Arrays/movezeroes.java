3ef0271911dc2b97c0c177c8f07e7cc51809040b
Siddesh Patil <siddeshpatil020@gmail.com>
2024-06-21 23:50:35 +0530

    arrays

--- Arrays/movezeroes.java
+++ Arrays/movezeroes.java
@@ -1,3 +1,24 @@
+//how i thought it should be
+class Solution {
+public void moveZeroes(int[] nums) {
+for(int i=0; i< nums.length; i++) {
+if(nums[i]==0) {
+swap(nums[i],nums[i+1]);
+}
+else if(nums[i]==0 && nums[i+1]==0) {
+swap(nums[i+2],nums[i]);
+        }
+    }
+    
+    
+}
+swap(int[] , int[]) {
+        temp = a;
+        a = b;
+        b= temp;}
+}
+
+
 class Solution {
     public void moveZeroes(int[] nums) {
         int lastNonZeroFoundAt = 0;
@@ -21,5 +42,22 @@ class Solution {
         int temp = nums[a];
         nums[a] = nums[b];
         nums[b] = temp;
+    }
+}
+
+
+//leetcode soln understood entrie
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int p=0;
+        
+        for(int n : nums)
+            if(n!=0)
+                nums[p++]=n;
+        
+        while(p<nums.length)
+            nums[p++]=0;
+        
     }
 }
