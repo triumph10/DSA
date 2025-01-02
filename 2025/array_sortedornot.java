b421f894eb9c4ab65b072fa3ad5ef878eb154c84
Siddesh Patil <siddeshpatil020@gmail.com>
2025-01-02 22:46:02 +0530

    .

---
+++ 2025/array_sortedornot.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int countBreaks = 0; 
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                countBreaks++;
+            }
+        }
+
+        return countBreaks <= 1;
+    }
+    
+}
